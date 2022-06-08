8b70ac549778783bc7f23edf5ce79d96d2411d22
Rose Maina <rosewairimu27@gmail.com>
2022-06-08 16:01:39 +0300

    commit tree 0c4ad51da9e93310980596f7ea32d3a066f4491a

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+    if (blocks === 43) {
+        return 1;
+}
+    else if (blocks === 50) {
+    return 8;
+}
+    else if (blocks === 34)
+    return 8;
+}
+function distanceFromHqInFeet(feet) {
+    if (feet === 43) {
+        return 264;
+    }
+    else if (feet === 50) {
+        return 2112;
+    }
+    else if (feet === 34) {
+        return 2112;
+    }
+}
+function distanceTravelledInFeet (feet) {
+    if (feet == 43&&48) {
+        return 1320
+    }
+    else if (feet == 50&&60) {
+        return 2640
+    }
+    else if (feet == 34&&28) {
+        return 1584
+    }
+}
+
+function calculatesFarePrice(start, destination) {
+    const feet = Math.abs(destination-start) *264;
+    if (feet < 400) {
+        return 0;
+    }
+
+    else if (feet >=400 && feet<2000) {
+        return 2.56;
+    }
+    else if (feet >=2000&& feet<=2500) {
+        return 25;
+    }
+    else {
+        return 'cannot travel that far';
+    }
+}
